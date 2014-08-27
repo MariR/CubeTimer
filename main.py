e313ba81a28a5bf1daa6680c8eabf420cc836360
MariR <mariusreisch@hotmail.de>
2014-08-27 20:42:37 +0200

    Changed button input to space-key input and added some loops

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Tkinter import *
-from time import *
 from sys import *
 from os import *
+import time
 
+
+global inprocess
+inprocess = 0
 
 # -------------GUI ------------------
 
@@ -10,50 +13,68 @@ def main():
 	root = Tk()
 	frame()
 	root.mainloop()
-	read_filelist()
 
 def frame():
 	f = Frame()
+	f.bind('<space>',space_event)
+	f.focus_set()
 	f.grid()
 
-	def instert_time(l_num, content):
-		time_list.insert(l_num, content)
+	label("Cube Timer",1,1,23)
+	label("Press Space to start the Timer",2,1,12)
 
-	label("Timer text here",1,1)
-	button("start",2,1,start_time)
-	button("reload files",2,2,read_filelist)
-
-	time_list = Listbox()
-	time_list.grid(row=1, column=2)
-	instert_time(1, "entry01")
-	instert_time(2, "entry02")
-
-
-
-def label(_text, _row, _column):
-	l = Label(text=str(_text))
+def label(_text, _row, _column, _fontsize):
+	l = Label(text=str(_text), font=("Arial", _fontsize))
 	l.grid(row=_row, column=_column)
 
 def button(_text, _row, _column, _command):
 	b = Button(text=str(_text), command = _command)
 	b.grid(row=_row, column=_column)
 
+def start_button(start):
+	
+	global start_button_text
+	
+	if start == True:
+		start_button_text = "Stop"
+	if start == False:
+		start_button_text = "Start"
+	
+
 # ------------------Timer Programm ---------
 
 
 def final_time(start_time, end_time):
 
+	global final_time
 	final_time = end_time - start_time
 	print final_time
-	return final_time
+
+def space_event(event):
+	wire()
+
+def wire():
+
+	global inprocess
+	inprocess = inprocess + 1
+
+	if inprocess == 1:
+		start_time()
+
+	if inprocess == 2:
+		end_time()
+
 
 def start_time():
 	start_time = time.time()
+	print "Timer started..."
 	return start_time
 
 def end_time():
 	end_time = time.time()
+	print "... Timer stopt."
 	return end_time
+
 	final_time()
 
 def date():
@@ -66,9 +87,5 @@ def write(file_name, final_time, date):
 	temp_data.write(content)
 	temp_data.close()
 	print "time saved"
-
-def read_filelist():
-	for f in /timelists:
-		print f
 
 main()
